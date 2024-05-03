bf5a295f589e4c2814dcf945543b13da5e02f778
Developer_Sameer <105971029+sam12-4@users.noreply.github.com>
2024-05-03 06:12:20 +0500

    Next Js File Based Routing

---
+++ Day37_NextJS_Intro/firstnextapp/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    
+    <div>
+      Hi I am about
+    </div>
+  )
+}
+
+export default page
---
+++ Day37_NextJS_Intro/firstnextapp/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>Hi i am a page
+    <ul>
+      Problems solved by next js
+      <li>Full Stack Solution</li>
+      <li>File Based Routing</li>
+      <li>Additional Features like routers from next/navigation</li>  
+      <li>Optimized Learning</li>
+    </ul></>
+  )
+}
